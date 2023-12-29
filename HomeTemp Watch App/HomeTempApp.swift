bb601b3e3188cf696da261436b9c82a448e9317c
P. Kurt Thorderson <pkthor@gmail.com>
2023-12-29 08:19:33 -0700

    Initial Commit

---
+++ HomeTemp Watch App/HomeTempApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeTempApp.swift
+//  HomeTemp Watch App
+//
+//  Created by P. Kurt Thorderson on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeTemp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
